96eef395cec5b7bc69b247f4e567aec759e923ab
unknown <sagarmikeylevi@gmail.com>
2023-06-04 21:38:21 +0530

    Started...

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Portfolio from "./components/Portfolio";
+
+function App() {
+  return (
+    <>
+      <Portfolio />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,5 @@
+const Portfolio = () => {
+    return <h1 className="text-xl text-red-600">Hello</h1>
+}
+
+export default Portfolio;
